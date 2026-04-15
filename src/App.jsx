caf3ef1ad3cf523270883331909f9ed2d19259c4
A K Biplob <akbiplobx@gmail.com>
2026-04-14 22:33:27 -0700

    banner and stats added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner"; 
+import StatsCards from "./components/StatsCards"; 
 import FriendsDashboard from "./components/FriendsDashboard";
-
 import friendsData from "./data/friends.json"; 
+
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <StatsCards />
+      <FriendsDashboard friends={friendsData} />
+    </>
+  );
+};
 
 function App() {
   return (
@@ -13,13 +25,12 @@ function App() {
 
         <main className="flex-grow">
           <Routes>
-         
-            <Route 
-              path="/" 
-              element={<FriendsDashboard friends={friendsData} />} 
-            />
             
+            <Route path="/" element={<Home />} />
             
+           
+            <Route path="/timeline" element={<div>Timeline Coming Soon...</div>} />
+            <Route path="/stats" element={<div>Detailed Stats Coming Soon...</div>} />
           </Routes>
         </main>
 
